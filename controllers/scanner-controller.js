64c1c7f90df1c323b20766e39fdedcb027ee763b
taherbadri <badri.taher@yahoo.com>
2024-01-31 15:32:02 +0530

    minor bug fix

--- controllers/scanner-controller.js
+++ controllers/scanner-controller.js
@@ -5,14 +5,13 @@ const { StatusCodes } = require("http-status-codes");
 
 const markAttendence = async (req, res) => {
 	const { id } = req.body;
-	const user = await User.find({ _id: id });
-	if (user.length === 0) {
+	const [{ sabeel, name }] = await User.find({ _id: id });
+	if (!name) {
 		throw new errors.BadRequestError("User does not exist");
 	}
-	const sabeel = user[0].sabeel;
 	const attendence = await Attendence.find({ sabeel });
 	if (attendence.length !== 0) {
-		throw new errors.BadRequestError("Attendence marked for today");
+		throw new errors.BadRequestError(`${name} Attendence marked for today`);
 	}
 	await Attendence.create({ sabeel, markedAt: new Date() });
 	res.status(StatusCodes.OK).json({ msg: "Attendence marked successfully" });
